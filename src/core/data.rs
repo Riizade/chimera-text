a3fdd538fb13dc19da3e475ffa57bfdcef8f2eb9
Adam <riizade@gmail.com>
2024-06-25 12:47:42 -0400

    updated deps, started skeleton

---
+++ src/core/data.rs
@@ -0,0 +1,6 @@
+/// associates a particular location in the text with a unique identifier
+/// this is useful to match corresponding locations between two parallel texts
+pub struct AnchorPoint {
+    identifier: String,
+    location: (), // TODO
+}
